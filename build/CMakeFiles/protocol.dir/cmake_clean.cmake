09041ab3c80f0c7b2d8bbae0799c0d35b4f9370a
Esteban Donis <estebandonis10@gmail.com>
2024-05-25 16:20:33 -0600

    Added build folder

---
+++ build/CMakeFiles/protocol.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/protocol.dir/src/protocol/chat.pb.cc.o"
+  "CMakeFiles/protocol.dir/src/protocol/chat.pb.cc.o.d"
+  "CMakeFiles/protocol.dir/src/protocol/message.cpp.o"
+  "CMakeFiles/protocol.dir/src/protocol/message.cpp.o.d"
+  "libprotocol.a"
+  "libprotocol.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/protocol.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
